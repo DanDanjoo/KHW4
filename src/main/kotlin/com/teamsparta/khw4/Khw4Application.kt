cede62067ed819b8b432d8b8a3d0200711731206
DanDanjoo <murekal94@gmail.com>
2024-05-20 10:16:30 +0900

    Swagger 설정 완료 - open API 등록

---
+++ src/main/kotlin/com/teamsparta/khw4/Khw4Application.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.khw4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Khw4Application
+
+fun main(args: Array<String>) {
+    runApplication<Khw4Application>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/khw4/Khw4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.khw4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Khw4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
