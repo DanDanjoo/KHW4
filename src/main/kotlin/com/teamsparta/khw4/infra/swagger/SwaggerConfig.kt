b8c2e7af92d4ce77bcfbe7d53af0305263113597
DanDanjoo <murekal94@gmail.com>
2024-05-20 10:18:02 +0900

    Swagger 설정 완료 - open API 등록

---
+++ src/main/kotlin/com/teamsparta/khw4/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.teamsparta.khw4.infra.swagger
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(
+            Info()
+                .title("Todo API")
+                .description("This API is used to determine a todo API")
+                .version("1.0.0")
+        )
+}
